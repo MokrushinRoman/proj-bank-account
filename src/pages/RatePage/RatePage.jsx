9c83ad82a03c5031312bb699544185a112bdf5d9
Sergey Opanasenko <svopanasenko22@gmail.com>
2023-04-15 16:12:44 +0200

    add rates

--- src/pages/RatePage/RatePage.jsx
+++ src/pages/RatePage/RatePage.jsx
@@ -1,4 +1,14 @@
+import { rates } from 'redux/operations';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+
 const RatePage = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(rates());
+  }, [dispatch]);
+
   return (
     <>
       <h2>RatePage</h2>
